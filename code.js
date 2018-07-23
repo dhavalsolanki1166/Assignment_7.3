bd6adb1fe3dc1551bc401a6e8bedf7d5c3cca54e
dhavalsolanki1166 <39427842+dhavalsolanki1166@users.noreply.github.com>
2018-07-23 18:32:58 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,14 @@
+var r="FizzBuzz";
+var a="Fizz";
+var d="Buzz";
+var i;
+for(i=1;i<=100;i++)
+{
+    console.log(i);
+    if(i%3==0 && i%5==0)
+    console.log(r);
+    else if(i%3==0)
+    console.log(a);
+    else if(i%5==0)
+    console.log(d);
+}
